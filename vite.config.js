1af16ae734953130f222faaa739778491945c511
Luiz Carlos de Siqueira Brandão <110572780+luiz-carloss@users.noreply.github.com>
2025-11-07 17:29:40 +0000

    Adicionado deploy para Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa' // 1. Importamos o plugin
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+base: '/apostilajufra/',
+
   plugins: [
     react(),
-    // 2. Adicionamos o plugin PWA
     VitePWA({
-      registerType: 'autoUpdate', // Ele vai se atualizar sozinho
-
-      // 3. O "manifest" diz ao celular como o app se chama e qual ícone usar
+      registerType: 'autoUpdate',
       manifest: {
         name: 'APPostila Jufra',
         short_name: 'APPostila',
         description: 'Apostila digital do coral Jufra de Lagoa Formosa.',
-        theme_color: '#ffffff', // Cor da barra de navegação do app
+        theme_color: '#ffffff',
         icons: [
           {
-            // Vamos usar o ícone do Vite que já está na pasta 'public'
             src: 'vite.svg',
             sizes: 'any',
             type: 'image/svg+xml'
